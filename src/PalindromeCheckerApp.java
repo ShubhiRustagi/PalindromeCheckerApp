06bceb0829d91042676e5bfb2db71c7eaad0eb1b
ShubhiRustagi <rustagishubhi20@gmail.com>
2026-02-16 11:05:03 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+/*
+*@author Shubhi
+*@version 1.0
+*
+*/
+   public static void main() {
+       System.out.println("PalindromeCheckerApp");
+       System.out.println("Version : 1.0");
+       System.out.println("System initialised successfully");
+
+
+    }
+}
